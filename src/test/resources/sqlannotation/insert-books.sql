8f0f6a85f4111b4a2aea5fc0e838d136246e1319
カワラ <life_enjoy.828@ezweb.ne.jp>
2024-03-18 12:36:26 +0900

    BoookMapperのfindAllのDBテストを実装

---
+++ src/test/resources/sqlannotation/insert-books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (book_id, name, reLease_date, is_purchased, category_id) VALUES (1, "ノーゲーム・ノーライフ・1", "2012/04/30", 1, 2);
+INSERT INTO books (book_id, name, reLease_date, is_purchased, category_id) VALUES (2, "鬼滅の刃・1", "2016/06/08", 0, 1);
+INSERT INTO books (book_id, name, reLease_date, is_purchased, category_id) VALUES (3, "ビブリア古書堂の事件手帖・1","2011/03/25",1,3);
