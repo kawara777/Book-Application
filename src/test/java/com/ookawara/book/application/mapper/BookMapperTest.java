367160de08a740016d72af4997efd0b16fb3ff86
カワラ <life_enjoy.828@ezweb.ne.jp>
2024-03-18 15:04:31 +0900

    findAllのDBテストを実装

---
+++ src/test/java/com/ookawara/book/application/mapper/BookMapperTest.java
@@ -0,0 +1,51 @@
+package com.ookawara.book.application.mapper;
+
+import com.ookawara.book.application.entity.Book;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookMapperTest {
+
+    @Autowired
+    BookMapper bookMapper;
+
+    @Test
+    @Sql(
+            scripts = {"classpath:/sqlannotation/delete-categories.sql", "classpath:/sqlannotation/insert-categories.sql",
+                    "classpath:/sqlannotation/delete-books.sql", "classpath:/sqlannotation/insert-books.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Transactional
+    void すべての本のデータが取得できること() {
+        List<Book> users = bookMapper.findAll();
+        assertThat(users)
+                .hasSize(3)
+                .contains(
+                        new Book(1, "ノーゲーム・ノーライフ・1", LocalDate.of(2012, 4, 30), true, 2, "ライトノベル"),
+                        new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1, "漫画"),
+                        new Book(3, "ビブリア古書堂の事件手帖・1", LocalDate.of(2011, 3, 25), true, 3, "小説")
+                );
+    }
+
+    @Test
+    @Sql(
+            scripts = {"classpath:/sqlannotation/delete-categories.sql", "classpath:/sqlannotation/delete-books.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Transactional
+    void レコードが存在しない時に空のデータが返されること() {
+        List<Book> books = bookMapper.findAll();
+        assertThat(books).isEmpty();
+    }
+}
---
+++ src/test/resources/sqlannotation/insert-books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (book_id, name, reLease_date, is_purchased, category_id) VALUES (1, "ノーゲーム・ノーライフ・1", "2012/04/30", true, 2);
+INSERT INTO books (book_id, name, release_date, is_purchased, category_id) VALUES (2, "鬼滅の刃・1", "2016/06/08", false, 1);
+INSERT INTO books (book_id, name, release_date, is_purchased, category_id) VALUES (3, "ビブリア古書堂の事件手帖・1", "2011/03/25", true, 3);
