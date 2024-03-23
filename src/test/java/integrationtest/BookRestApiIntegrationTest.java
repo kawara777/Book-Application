32653ff95fd1ad6133c2765e1978aed799c19af5
カワラ <life_enjoy.828@ezweb.ne.jp>
2024-03-23 16:38:26 +0900

    GetMappingの/book/BookIdに対する結合テスト

--- src/test/java/integrationtest/BookRestApiIntegrationTest.java
+++ src/test/java/integrationtest/BookRestApiIntegrationTest.java
@@ -4,6 +4,10 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.ookawara.book.application.BookApplication;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
+import java.time.ZonedDateTime;
 
 @SpringBootTest(classes = BookApplication.class)
 @AutoConfigureMockMvc
@@ -169,5 +176,44 @@ class BookRestApiIntegrationTest {
                         []
                         """
                 ));
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 存在する本のIDを指定したときに正常に本のデータを返すこと() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/book/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "bookId": 1,
+                            "name": "ノーゲーム・ノーライフ・1",
+                            "releaseDate": 2012-04-30,
+                            "isPurchased": true,
+                            "categoryId": 2,
+                            "category": "ライトノベル"
+                        }
+                        """
+                ));
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 存在しない本のIDを指定したときにステータスコードが404となり例外メッセージが返されること() throws Exception {
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/book/0"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                {
+                    "timestamp": "2024-01-01 00:00:00.000000+09:00[Asia/Tokyo]",
+                    "status": "404",
+                    "error": "Not Found",
+                    "message": "book：0 のデータはありません。",
+                    "path": "/book/0"
+                }
+                """, response, new CustomComparator(JSONCompareMode.STRICT, new Customization("timestamp", (o1, o2) -> true
+        )));
     }
 }
