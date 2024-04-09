2ed236b6c38be76f4ae1b1ec4cffda4629b4ce5a
カワラ <life_enjoy.828@ezweb.ne.jp>
2024-04-10 00:17:23 +0900

    DBテストのテスト内容を追加

--- src/main/java/com/ookawara/book/application/mapper/BookSqlProvider.java
+++ src/main/java/com/ookawara/book/application/mapper/BookSqlProvider.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.builder.annotation.ProviderMethodResolver;
 import org.apache.ibatis.jdbc.SQL;
 
+import java.time.LocalDate;
+
 public class BookSqlProvider implements ProviderMethodResolver {
     public String findBy(@Param("category") String category,
                          @Param("name") String name,
@@ -14,10 +16,10 @@ public class BookSqlProvider implements ProviderMethodResolver {
                 SELECT("*");
                 FROM("books");
                 JOIN("categories on books.category_id = categories.category_id");
-                if (category != null) {
+                if (category != null && !category.isEmpty()) {
                     WHERE("category like concat('%',#{category},'%')");
                 }
-                if (name != null) {
+                if (name != null && !name.isEmpty()) {
                     WHERE("name like concat('%',#{name},'%')");
                 }
                 if (isPurchased != null) {
@@ -31,7 +33,7 @@ public class BookSqlProvider implements ProviderMethodResolver {
         return new SQL() {
             {
                 UPDATE("books");
-                if (book.getName() != null) {
+                if (book.getName() != null && !book.getName().isBlank()) {
                     SET("name = #{name}");
                 }
                 if (book.getReleaseDate() != null) {
@@ -40,7 +42,7 @@ public class BookSqlProvider implements ProviderMethodResolver {
                 if (book.getIsPurchased() != null) {
                     SET("is_purchased = #{isPurchased}");
                 }
-                if (book.getCategoryId() != null) {
+                if (book.getCategoryId() >= 1) {
                     SET("category_id = #{categoryId}");
                 }
                 WHERE("book_id = #{bookId}");
@@ -48,4 +50,3 @@ public class BookSqlProvider implements ProviderMethodResolver {
         }.toString();
     }
 }
-
--- src/test/java/com/ookawara/book/application/mapper/BookMapperTest.java
+++ src/test/java/com/ookawara/book/application/mapper/BookMapperTest.java
@@ -128,8 +128,72 @@ class BookMapperTest {
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
     )
     @Transactional
-    void 指定した文字列を含むデータが存在しないとき空のデータを返す() {
-        List<Book> books = bookMapper.findBy(null, " ", null);
+    void 全てにnullを指定したときsql文の条件により全ての本のデータを返すこと() {
+        List<Book> books = bookMapper.findBy(null, null, null);
+        assertThat(books)
+                .hasSize(3)
+                .containsOnly(
+                        new Book(1, "ノーゲーム・ノーライフ・1", LocalDate.of(2012, 4, 30), true, 2, "ライトノベル"),
+                        new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1, "漫画"),
+                        new Book(3, "ビブリア古書堂の事件手帖・1", LocalDate.of(2011, 3, 25), true, 3, "小説")
+                );
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void カテゴリーに空文字を書籍名と購入状況にnullを指定したときsql文の条件により全ての本のデータを返すこと() {
+        List<Book> books = bookMapper.findBy("", null, null);
+        assertThat(books)
+                .hasSize(3)
+                .containsOnly(
+                        new Book(1, "ノーゲーム・ノーライフ・1", LocalDate.of(2012, 4, 30), true, 2, "ライトノベル"),
+                        new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1, "漫画"),
+                        new Book(3, "ビブリア古書堂の事件手帖・1", LocalDate.of(2011, 3, 25), true, 3, "小説")
+                );
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 書籍名に空文字をカテゴリーと購入状況にnullを指定したときsql文の条件により全ての本のデータを返すこと() {
+        List<Book> books = bookMapper.findBy(null, "", null);
+        assertThat(books)
+                .hasSize(3)
+                .containsOnly(
+                        new Book(1, "ノーゲーム・ノーライフ・1", LocalDate.of(2012, 4, 30), true, 2, "ライトノベル"),
+                        new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1, "漫画"),
+                        new Book(3, "ビブリア古書堂の事件手帖・1", LocalDate.of(2011, 3, 25), true, 3, "小説")
+                );
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 書籍名とカテゴリーに空文字を購入状況にnullを指定したときsql文の条件により全ての本のデータを返すこと() {
+        List<Book> books = bookMapper.findBy("", "", null);
+        assertThat(books)
+                .hasSize(3)
+                .containsOnly(
+                        new Book(1, "ノーゲーム・ノーライフ・1", LocalDate.of(2012, 4, 30), true, 2, "ライトノベル"),
+                        new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1, "漫画"),
+                        new Book(3, "ビブリア古書堂の事件手帖・1", LocalDate.of(2011, 3, 25), true, 3, "小説")
+                );
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void カテゴリーに存在しない文字列を指定したとき空のデータを返すこと() {
+        List<Book> books = bookMapper.findBy(" ", "", null);
+        assertThat(books).isEmpty();
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 書籍名に存在しない文字列を指定したとき空のデータを返すこと() {
+        List<Book> books = bookMapper.findBy("", " ", null);
         assertThat(books).isEmpty();
     }
 
@@ -172,7 +236,7 @@ class BookMapperTest {
     @Test
     @DataSet("datasets/books.yml")
     @Transactional
-    void 指定した値が完全一致した本のデータを返す() {
+    void 指定した全てのレコードが完全一致した本のデータを返す() {
         Optional<Book> book = bookMapper.findBook("鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1);
         assertThat(book).contains(new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1));
     }
@@ -180,8 +244,40 @@ class BookMapperTest {
     @Test
     @DataSet("datasets/books.yml")
     @Transactional
-    void 指定した値がどれか一つでも一致しないとき空のデータを返す() {
-        Optional<Book> book = bookMapper.findBook("鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 0);
+    void 指定した全てのレコードが一致しないとき空のデータを返す() {
+        Optional<Book> book = bookMapper.findBook("", LocalDate.of(2000, 1, 1), null, 0);
+        assertThat(book).isEmpty();
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 書籍名のみ指定したとき空のデータを返す() {
+        Optional<Book> book = bookMapper.findBook("鬼滅の刃・1", null, null, 0);
+        assertThat(book).isEmpty();
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 発売日のみ指定したとき空のデータを返す() {
+        Optional<Book> book = bookMapper.findBook(" ", LocalDate.of(2016, 6, 8), null, 0);
+        assertThat(book).isEmpty();
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void 購入状況のみ指定したとき空のデータを返す() {
+        Optional<Book> book = bookMapper.findBook(null, null, false, 0);
+        assertThat(book).isEmpty();
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @Transactional
+    void カテゴリーIDのみ指定したとき空のデータを返す() {
+        Optional<Book> book = bookMapper.findBook(" ", null, null, 1);
         assertThat(book).isEmpty();
     }
 
@@ -241,7 +337,7 @@ class BookMapperTest {
     @ExpectedDataSet(value = "datasets/update/update-books-name.yml")
     @Transactional
     void IDで指定した書籍の名前のみが更新できること() {
-        Book book = new Book(2, "鬼滅の刃 1", null, false, null);
+        Book book = new Book(2, "鬼滅の刃 1", null, null, 0);
         bookMapper.updateBook(book);
     }
 
@@ -250,7 +346,7 @@ class BookMapperTest {
     @ExpectedDataSet(value = "datasets/update/update-books-releaseDate.yml")
     @Transactional
     void IDで指定した書籍の発売日のみが更新できること() {
-        Book book = new Book(2, null, LocalDate.of(2016, 7, 8), false, null);
+        Book book = new Book(2, null, LocalDate.of(2016, 7, 8), null, 0);
         bookMapper.updateBook(book);
     }
 
@@ -259,7 +355,7 @@ class BookMapperTest {
     @ExpectedDataSet(value = "datasets/update/update-books-isPurchased.yml")
     @Transactional
     void IDで指定した書籍の購入履歴のみが更新できること() {
-        Book book = new Book(2, null, null, true, null);
+        Book book = new Book(2, "", null, true, 0);
         bookMapper.updateBook(book);
     }
 
@@ -268,7 +364,25 @@ class BookMapperTest {
     @ExpectedDataSet(value = "datasets/update/update-books-categoryId.yml")
     @Transactional
     void IDで指定した書籍のカテゴリーIDのみが更新できること() {
-        Book book = new Book(2, null, null, false, 2);
+        Book book = new Book(2, " ", null, null, 2);
+        bookMapper.updateBook(book);
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @ExpectedDataSet("datasets/books.yml")
+    @Transactional
+    void IDで指定した書籍のデータと同じ値を更新するときデータが更新されないこと() {
+        Book book = new Book(2, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1);
+        bookMapper.updateBook(book);
+    }
+
+    @Test
+    @DataSet("datasets/books.yml")
+    @ExpectedDataSet("datasets/books.yml")
+    @Transactional
+    void IDで指定した書籍が存在しないときデータが更新されないこと() {
+        Book book = new Book(4, "鬼滅の刃・1", LocalDate.of(2016, 6, 8), false, 1);
         bookMapper.updateBook(book);
     }
 }
