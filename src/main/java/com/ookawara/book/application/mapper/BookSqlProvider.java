ab5d6a0221ba42187bf6032f6515c0888c4dbde7
カワラ <life_enjoy.828@ezweb.ne.jp>
2024-04-10 18:44:51 +0900

    修正２

--- src/main/java/com/ookawara/book/application/mapper/BookSqlProvider.java
+++ src/main/java/com/ookawara/book/application/mapper/BookSqlProvider.java
@@ -42,10 +42,8 @@ public class BookSqlProvider implements ProviderMethodResolver {
                 if (book.getIsPurchased() != null) {
                     SET("is_purchased = #{isPurchased}");
                 }
-                if (book.getCategoryId() != null && book.getCategoryId() >= 1) {
+                if (book.getCategoryId() >= 1) {
                     SET("category_id = #{categoryId}");
-                } else {
-                    throw new IllegalArgumentException();
                 }
                 WHERE("book_id = #{bookId}");
             }
